74e75740d26f45948bc1da0ea99498ee6222ae20
Srikanth Sureddy <sureddy1@outlook.com>
2018-05-15 11:11:24 -0500

    Initial Commit
    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # l200_hc_templ
+
+
+# Deployment of a Windows VM, configure windows featurtes like IIS, .Net framework etc., download application deployment packages using DSC
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2Fazure-quickstart-templates%2Fmaster%2F201-vm-win-iis-app-ssl%2Fazuredeploy.json" target="_blank">
+<img src="https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/201-vm-win-iis-app-ssl/images/deploytoazure.png"/>
+</a>
+<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2Fazure-quickstart-templates%2Fmaster%2F201-vm-win-iis-app-ssl%2Fazuredeploy.json" target="_blank">
+<img src="https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/201-vm-win-iis-app-ssl/images/visualizebutton.png"/>
+</a>
+
+To deploy this template using the scripts from the root of this repo: 
+```PowerShell
+
+.\Deploy-AzureResourceGroup.ps1 -StorageAccountName '<artifacts storage account name>' -ResourceGroupName '<Resource guroup name>' -ResourceGroupLocation '<RG location>' -TemplateFile .\azuredeploy.json -TemplateParametersFile .\azuredeploy.parameters.json -ArtifactStagingDirectory '.' -DSCSourceFolder '.\dsc' -UploadArtifacts
+```
+
+The following resources are deployed as part of the solution
+
+#### A VNet with a single subnet 
+The VNet and the subnet IP prefixes are defined in the variables section i.e. appVnetPrefix & appVnetSubnet1Prefix respectively. Set these two accrodingly.
+
+#### NSG to define the security rules
+It defines the rules for http, https and rdp acces to the VM. The NSG is assigned to the subnet
+
+#### A NIC, a Public IP and a VM with Windows Server 2012 R2
+
+#### A Storage account for the VM as well as for the artifacts
+
+## Deployment steps
+
+You can click the "deploy to Azure" button at the beginning of this document or follow the instructions for command line deployment using the scripts in the root of this repo.
---
+++ dsc/appDSC.ps1
@@ -0,0 +1,144 @@
+Configuration Main
+{
+
+Param ( [string] $nodeName, [string] $webDeployPackage, [string] $certStoreName, [string]  $certDomain)
+
+Import-DscResource -ModuleName PSDesiredStateConfiguration
+Import-DscResource -ModuleName WebAdministration
+
+Node $nodeName
+  {
+   # This commented section represents an example configuration that can be updated as required.
+    WindowsFeature WebServerRole
+    {
+      Name = "Web-Server"
+      Ensure = "Present"
+    }
+    WindowsFeature WebManagementConsole
+    {
+      Name = "Web-Mgmt-Console"
+      Ensure = "Present"
+    }
+    WindowsFeature WebManagementService
+    {
+      Name = "Web-Mgmt-Service"
+      Ensure = "Present"
+    }
+    WindowsFeature ASPNet45
+    {
+      Name = "Web-Asp-Net45"
+      Ensure = "Present"
+    }
+    WindowsFeature HTTPRedirection
+    {
+      Name = "Web-Http-Redirect"
+      Ensure = "Present"
+    }
+    WindowsFeature CustomLogging
+    {
+      Name = "Web-Custom-Logging"
+      Ensure = "Present"
+    }
+    WindowsFeature LogginTools
+    {
+      Name = "Web-Log-Libraries"
+      Ensure = "Present"
+    }
+    WindowsFeature RequestMonitor
+    {
+      Name = "Web-Request-Monitor"
+      Ensure = "Present"
+    }
+    WindowsFeature Tracing
+    {
+      Name = "Web-Http-Tracing"
+      Ensure = "Present"
+    }
+    WindowsFeature BasicAuthentication
+    {
+      Name = "Web-Basic-Auth"
+      Ensure = "Present"
+    }
+    WindowsFeature WindowsAuthentication
+    {
+      Name = "Web-Windows-Auth"
+      Ensure = "Present"
+    }
+    WindowsFeature ApplicationInitialization
+    {
+      Name = "Web-AppInit"
+      Ensure = "Present"
+    }
+    Script DownloadWebDeploy
+    {
+        TestScript = {
+            Test-Path "C:\WindowsAzure\WebDeploy_amd64_en-US.msi"
+        }
+        SetScript ={
+            $source = "https://download.microsoft.com/download/0/1/D/01DC28EA-638C-4A22-A57B-4CEF97755C6C/WebDeploy_amd64_en-US.msi"
+            $dest = "C:\WindowsAzure\WebDeploy_amd64_en-US.msi"
+            Invoke-WebRequest $source -OutFile $dest
+        }
+        GetScript = {@{Result = "DownloadWebDeploy"}}
+        DependsOn = "[WindowsFeature]WebServerRole"
+    }
+    Package InstallWebDeploy
+    {
+        Ensure = "Present"  
+        Path  = "C:\WindowsAzure\WebDeploy_amd64_en-US.msi"
+        Name = "Microsoft Web Deploy 3.6"
+        ProductId = "{ED4CC1E5-043E-4157-8452-B5E533FE2BA1}"
+        Arguments = "ADDLOCAL=ALL"
+        DependsOn = "[Script]DownloadWebDeploy"
+    }
+    Service StartWebDeploy
+    {                    
+        Name = "WMSVC"
+        StartupType = "Automatic"
+        State = "Running"
+        DependsOn = "[Package]InstallWebDeploy"
+    } 
+	# Install the IIS role 
+		#WindowsFeature IIS 
+		#{ 
+		#	Ensure          = "Present" 
+		#	Name            = "Web-Server" 
+		#} 
+		## Install the ASP .NET 4.5 role 
+		#WindowsFeature AspNet45 
+		#{ 
+		#	Ensure          = "Present" 
+		#	Name            = "Web-Asp-Net45" 
+		#} 
+	   
+		
+		# Download and install the web site and content
+		Script DeployWebPackage
+		{
+			GetScript = {@{Result = "DeployWebPackage"}}
+			TestScript = {$false}
+			SetScript ={
+				[system.io.directory]::CreateDirectory("C:\WebApp")
+				$dest = "C:\WebApp\source.zip" 
+				Remove-Item -path "C:\inetpub\wwwroot" -Force -Recurse -ErrorAction SilentlyContinue
+				Invoke-WebRequest $using:webDeployPackage -OutFile $dest
+				Add-Type -assembly "system.io.compression.filesystem"
+				[io.compression.zipfile]::ExtractToDirectory($dest, "C:\inetpub\wwwroot")							
+			}
+			DependsOn  = "[WindowsFeature]WebServerRole"
+		}
+
+	
+		# Copy the website content 
+		File WebContent 
+		{ 
+			Ensure          = "Present" 
+			SourcePath      = "C:\WebApp"
+			DestinationPath = "C:\Inetpub\wwwroot"
+			Recurse         = $true 
+			Type            = "Directory" 
+			DependsOn       = "[Script]DeployWebPackage" 
+		}		
+		
+  }
+}
